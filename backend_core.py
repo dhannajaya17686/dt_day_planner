abbca09e941115ceb6b9908008f47b372140932b
Dhananjaya_Mudalige <71549074+dhannajaya17686@users.noreply.github.com>
2023-04-30 11:57:31 +0530

    ui_in_dev

--- backend_core.py
+++ backend_core.py
@@ -1,30 +1,37 @@
 class Backend_core_module():
-
-    def db_creator():
-        pass
-
-    def db_checker():
-        pass
     
-    def resources_checker():
+    def db_creator(db_name:str)->bool:
+        #other blocks go here
+        return True
+
+    def db_checker(db_name:str)->bool:
+        #the code blocks go here
+        return True
+    
+    def resources_checker(image_path:str)->bool:
+        #the code block goes here
+        return True
+
+    def normal_mode_table_creator(db_name:str,table_name:str,date:str,hour_list:list)->bool:
+        #the code block goes here
+        return True
+
+    def advanced_mode_creator(db_name:str,table_name:str,date:str,hour_list:list)->bool:
+        #the code block goes here
+        return True
+
+    def normal_mode_submitter(db_name:str,date:str,activity_dic:dict)->bool:
+        #the code block goes here
+        return True
+
+    def advanced_mode_submitter(db_name:str,date:str,activity_dic:dict)->bool:
+        #the code block goes here
+        return True
+    
+    def normal_mode_show(db_name:str,date:str)->list:
         pass
 
-    def normal_mode_table_creator():
-        pass
-
-    def advanced_mode_creator():
-        pass
-
-    def normal_mode_submitter():
-        pass
-
-    def advanced_mode_submitter():
-        pass
-
-    def normal_mode_show():
-        pass
-
-    def advanced_mode_show():
+    def advanced_mode_show(db_name:str,date:str)->list:
         pass
 
     
